3de7b1dfb6b714107254114046f9575f2be078bc
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-12-11 17:00:19 +0100

    Initialise C# Learning project

---
+++ TequilasRestaurant/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TequilasRestaurant.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public ICollection<Order>? Orders { get; set; }
+}
